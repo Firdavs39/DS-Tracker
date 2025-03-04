c589f3eed35fd895bafe1163168afaef14f3a66e
Decoroom Steel <legogsalat@gmail.com>
2025-03-04 11:53:21 +0500

    Новое

--- app/src/main/java/com/decoroomsteel/dstracker/data/AppDatabase.kt
+++ app/src/main/java/com/decoroomsteel/dstracker/data/AppDatabase.kt
@@ -1,6 +1,8 @@
 package com.decoroomsteel.dstracker.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.decoroomsteel.dstracker.data.dao.LocationDao
@@ -36,4 +38,23 @@ abstract class AppDatabase : RoomDatabase() {
      * DAO для работы с рабочими сменами
      */
     abstract fun sessionDao(): SessionDao
-} 
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "ds_tracker_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
